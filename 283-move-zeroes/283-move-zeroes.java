93d6f3d090d9306e0fb7cc3f5b7f7c2062dd007b
GAURAV SARKAR <31614476+gauravsarkar97@users.noreply.github.com>
2022-03-21 22:27:45 +0530

    Time: 2 ms (72.87%), Space: 54.2 MB (65.49%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,27 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        //using extra array
-        List<Integer> myList = new ArrayList<Integer>();
-        int zero_count = 0;
-        
-        //counting no. of zeros and adding non-zero elements to list
+        int lastNonZeroAt = 0;
         for(int i = 0; i< nums.length; i++){
-            if(nums[i] == 0)
-                zero_count++;
-            if(nums[i] != 0)
-                myList.add(nums[i]);
+            if(nums[i] != 0){
+                nums[lastNonZeroAt++] = nums[i];
+            }
         }
         
-        //adding all zeroes to end of list
-        while(zero_count != 0){
-            myList.add(0);
-            zero_count--;
-        }
-        
-        //iterating through list and assigning back to original array
-        int i = 0;
-        for(Integer n: myList){
-            nums[i++] = n;
+        //fill remaining with 0
+        for(int i = lastNonZeroAt; i< nums.length; i++){
+            nums[i] = 0;
         }
     }
 }

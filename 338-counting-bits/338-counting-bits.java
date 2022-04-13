d3c832aebb99fbc11e666f6d198c82c52011c07a
GAURAV SARKAR <31614476+gauravsarkar97@users.noreply.github.com>
2022-04-13 21:43:52 +0530

    Time: 1 ms (99.96%), Space: 46.4 MB (88.54%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -5,17 +5,8 @@ class Solution {
         if(n == 0)  return ans;
         
         for(int i = 1; i <= n; i++){
-            ans[i] = noOfBits(i);
+            ans[i] = ans[i/2] + (i % 2);
         }
         return ans;
-    }
-    
-    private int noOfBits(int n){
-        int count = 0;
-        while(n != 0){
-            n = n & (n-1);
-            count++;
-        }
-        return count;
     }
 }
